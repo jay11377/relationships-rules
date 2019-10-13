3e332023c05d5fa9d812e999021c3fc4ac49e45b
Jeremy <jeremy.thebault.jp@gmail.com>
2019-10-14 01:25:21 +0200

    RR-01: Correct logic rules

--- rules.php
+++ rules.php
@@ -10,10 +10,10 @@ if ($predator == 'female' && $target == 'male') {
 }
 else if ($predator == 'man' $target == 'female') {
 	if ($hot == TRUE && $smart == FALSE && $funny == FALSE && $pretty == FALSE) {
-		$wanna_get_married = TRUE;
+		$wanna_fuck = TRUE;
 	}
 	else if ($smart == FALSE && $funny == FALSE && $pretty == FALSE) {
-		$wanna_fuck = TRUE;
+		$wanna_get_married = TRUE;
 	}
 }
 else if ($predator == 'man' $target == 'man') {
